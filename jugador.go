fc6e326b7c0a2db65bc15aae6a2d8346aa027967
Alvaro <alvaro.fuentesm@sansano.usm.cl>
2021-11-03 20:12:14 -0300

    Jugador: switch case inicial

--- jugador.go
+++ jugador.go
@@ -2,6 +2,7 @@ package main
 import (
     "fmt"
     "os"
+	"bufio"
 )
 func main() {
 	argsWithoutProg := os.Args[1:]
@@ -10,7 +11,50 @@ func main() {
 	if len(argsWithoutProg) > 0 && argsWithoutProg[0] == "bot"{
 		bot = true
 	}
+
+	if bot {
+		fmt.Println("Bienvenid@ bot")
+	} else{
+		fmt.Println("Bienvenid@ jugador")
+	}
+
+	var continue_flag = true
+	var alive = false
+
 	
+	fmt.Println("Elija opción:")
+	fmt.Println("1. Unirse al juego del calamar\n2. Terminar todo")
+
+	for continue_flag {	
+		reader := bufio.NewReader(os.Stdin)
+		char, _, err := reader.ReadRune()
+	
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		switch char {
+			case '1':
+				alive = true
+				continue_flag = false
+			case '2':
+				continue_flag = false
+			default: 
+				fmt.Println("Respuesta no valida")
+		}
+	}
+
+	for alive {
+		fmt.Println("-----------Vivo----------")
+
+		// TO-DO: logica de juegos 
+		
+		break // TO-DO: logica de salida
+	}
+	fmt.Println("Finalizando proceso jugador")
+	
+	
+	/*
 	fmt.Println("Hola, mundo.")
 	fmt.Println("Bot? ", bot)
 
@@ -28,5 +72,8 @@ func main() {
 
 	f := "short"
 	fmt.Println(f)
+	*/
+
+	
 
 }
