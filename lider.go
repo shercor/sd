39ff7b5899b50ecd2011aa4b748d8f0befe74ea8
CJTrujillo <christian.trujillo@sansano.usm.cl>
2021-11-04 19:36:01 -0300

    Lider: etapa 1 lista (offline)

--- lider.go
+++ lider.go
@@ -22,15 +22,21 @@ func main() {
 */
 
 type jugador struct {
-	ID     int
-	IP     string
-	puerto string
-	estado string
+	ID                int
+	IP                string
+	puerto            string
+	estado            string
+	suma_rptas_etapa1 int
 	// equipo?
 }
 
 func newJugador(ID int, IP string, puerto string) *jugador { // Crea un struct jugador con ID, IP, puerto, etc
-	player := jugador{ID: ID, IP: IP, puerto: puerto, estado: "vivo"}
+	player := jugador{
+		ID:                ID,
+		IP:                IP,
+		puerto:            puerto,
+		estado:            "vivo",
+		suma_rptas_etapa1: 0}
 	return &player
 }
 
@@ -68,20 +74,16 @@ func informarPozo() { // Agrega 100 millones a Pozo cuando un jugador muere
 
 }
 
-func llamarInterfaz() { // Llama a la interfaz principal para iniciar el juego y demases
-
-}
-
-func mostrarMuerte() { // Muestra por consola cuando muere un jugador
-
-}
-
-func jugadoresVivos(lista_jugadores []jugador, cant_jugadores int) { // Muestra por consola a los jugadores vivos al término de cada etapa
+func jugadoresVivos(lista_jugadores []*jugador, cant_jugadores int, print bool) (lista_vivos []int) { // Muestra por consola a los jugadores vivos al término de cada etapa
 	for i := 0; i < cant_jugadores; i++ {
 		if lista_jugadores[i].estado == "vivo" {
-			fmt.Println("El jugador:", lista_jugadores[i].ID, "está vivo")
+			if print {
+				fmt.Println("El jugador:", lista_jugadores[i].ID, "está vivo")
+			}
+			lista_vivos = append(lista_vivos, lista_jugadores[i].ID)
 		}
 	}
+	return lista_vivos
 }
 
 func mostrarGanadores() { // Muestra por consola a los ganadores
@@ -92,10 +94,57 @@ func revisarJugadas() { // Para preguntar sobre las jugadas historicamente de un
 
 }
 
-func esEliminado(jugador_eliminado jugador) { // Cuando la logica detecta que el jugador muere, avisa al jugador y al pozo
-	// Enviar msj al jugador diciendo que murio
-	// Enviar msj al pozo para aumentarlo
+func esEliminado(jugador_eliminado jugador) { // Cuando la logica detecta que el jugador muere, avisa al jugador
 	jugador_eliminado.estado = "muerto"
+	mostrarMuerte(jugador_eliminado.ID) // Printea por consola quien murio
+}
+
+func mostrarMuerte(ID_player int) { // Muestra por consola cuando muere un jugador
+	fmt.Println("El jugador:", ID_player, "ha sido eliminado")
+}
+
+func informarResultadoRonda(muertos_por_ronda []int, pasan_etapa []int) { // Lee los ID de quienes murieron/pasaron y les manda mensaje, tambien al pozo
+	for i := 0; i < len(muertos_por_ronda); i++ {
+		var playerID = muertos_por_ronda[i]
+		// Mandar msj a jugador de ID playerID
+		// Mensaje de ejemplo:
+		fmt.Println("El jugador", playerID, "(tú) está eliminado")
+		// Mandar msj al pozo para aumentarlo
+	}
+	for i := 0; i < len(pasan_etapa); i++ {
+		var playerID = pasan_etapa[i]
+		// Mandar msj a jugador de ID playerID
+		// Mensajes de ejemplo
+		fmt.Println("El jugador", playerID, "(tú) pasa a la siguiente etapa")
+		fmt.Println("Esperando término de etapa...")
+	}
+}
+
+func evaluarRestantes(lista_vivos []int, lista_jugadores []*jugador) {
+	var num_vivos = len(lista_vivos)
+	if num_vivos == 0 {
+		// Mandar mensaje a ¿todos?, no hay ganadores
+		fmt.Println("Todos los jugadores fueron eliminados")
+		fmt.Println("No hay ganadores")
+		// Dar la orden para terminar el juego
+	} else if num_vivos == 1 {
+		// Mandar mensaje avisando quien gano
+		fmt.Println("Solo queda un jugador en pie")
+		fmt.Println("El jugador", lista_vivos[0], "ganó el juego del Calamar")
+		// Dar la orden para terminar el juego
+	} else {
+		// Mandar mensaje a los jugadores que pasan a la fase 2
+		fmt.Println("Los jugadores de ID:")
+		for i := 0; i < num_vivos; i++ {
+			// Mandar mensaje a los jugadores
+			fmt.Println(lista_vivos[i])
+		}
+		fmt.Println("pasan a la siguiente ronda")
+	}
+}
+
+func eliminarJugadorImpar(lista_jugadores []*jugador, ID_eliminado int) {
+	esEliminado(*lista_jugadores[ID_eliminado])
 }
 
 func main() {
@@ -103,16 +152,16 @@ func main() {
 
 	cant_jugadores := 16
 	argsWithoutProg := os.Args[1:]
-	
-	if len(argsWithoutProg) ==  1 {
-		cant_jugadores, err :=  strconv.Atoi(argsWithoutProg[0])
+
+	if len(argsWithoutProg) == 1 {
+		cant_jugadores, err := strconv.Atoi(argsWithoutProg[0])
 		if err != nil {
 			fmt.Println(err)
 		}
 		fmt.Println(cant_jugadores)
 	}
 
-	const wones = 100000000                      // 100 millones de wones vale cada jugador
+	const wones = 100000000 // 100 millones de wones vale cada jugador
 	//var lista_jugadores [cant_jugadores]*jugador // Deprecado: Lista de structs con los jugadores
 	var lista_jugadores []*jugador // Slice de structs con los jugadores
 
@@ -126,7 +175,7 @@ func main() {
 		// Guardar la IP y puerto recibidos
 
 		//lista_jugadores[i] = newJugador(i+1, "ip", "puerto") // Deprecado.
-		lista_jugadores = append(lista_jugadores, newJugador(i+1, "ip", "puerto") ) // Jugador 1, 2, 3, ..., 16
+		lista_jugadores = append(lista_jugadores, newJugador(i+1, "ip", "puerto")) // Jugador 1, 2, 3, ..., 16
 
 		// Aqui responde al jugador con el ID asignado
 
@@ -138,16 +187,19 @@ func main() {
 	var etapa = 1
 	const max_rondas = 4
 	var contador_rondas int = 0
+	var en_juego = jugadores_vivos
+
 	// LOOP ETAPA 1
 	// -------------
 	for contador_rondas < max_rondas {
 
 		var muertos_por_ronda []int // Una lista de muertos ([1,4,5,8,16] por ejemplo)
+		var pasan_etapa []int       // Una lista de quienes pasan la etapa, por ronda
 		// Lider escoge un numero al azar entre el 6 y 10
 		opt_lider := getRandomNum(6, 10)
 		fmt.Println("La opcion del Lider es:", opt_lider)
 
-		for i := 0; i < jugadores_vivos; i++ {
+		for i := 0; i < en_juego; i++ {
 
 			// Recoger primera respuesta que llegue
 			// Guardar el ID de esa respuesta
@@ -163,19 +215,31 @@ func main() {
 			} else {
 				// Mandar mensaje que el jugador sigue vivo
 				// Mostrar el numero que lleva acumulado
-				// Si la suma de sus respuestas es >= 21, informar que pasa a la siguiente etapa
-
-				// -------------------------------------------
-				// TO-DO
-				// VER LO DE LA SUMA DE RESPUESTAS, AGREGAR UN CAMPO AL STRUCT
-				// -------------------------------------------
+				lista_jugadores[ID_rpta-1].suma_rptas_etapa1 += rpta
+				if lista_jugadores[ID_rpta-1].suma_rptas_etapa1 >= 21 { // Si la suma de sus respuestas es >= 21, informar que pasa a la siguiente etapa
+					// Informar que pasa a la siguiente etapa
+					pasan_etapa = append(pasan_etapa, ID_rpta) // Como ya paso, no se calcula en los que faltan
+				}
 			}
 			// Registrar su jugada en NameNode
 		}
 
-		jugadores_vivos = jugadores_vivos - len(muertos_por_ronda)
-		contador_rondas = contador_rondas + 1
+		jugadores_vivos -= len(muertos_por_ronda)
+		en_juego -= len(muertos_por_ronda)
+		en_juego -= len(pasan_etapa)
+		contador_rondas += 1
+
+		// Leer slices e informar quienes pasan o no
+		informarResultadoRonda(muertos_por_ronda, pasan_etapa)
 	}
+	// Chequear si hay jugadores vivos para seguir jugando, y quienes
+	var lista_vivos = jugadoresVivos(lista_jugadores, cant_jugadores, false)
+	evaluarRestantes(lista_vivos, lista_jugadores)
+
+	// Inicio de etapa 2
+	// Primero aqui avisa a los jugadores que iniciara la etapa 1 y que manden sus respuestas
 
 	etapa = etapa + 1
+	// Ver si los restantes son par
+
 }
